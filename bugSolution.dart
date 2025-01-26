2696968f89016e41ca16e1027d69108c3bac1536
BugHunter-X <askzenai@gmail.com>
2025-01-26 12:08:07 +0100

    Add bugSolution.dart file

---
+++ bugSolution.dart
@@ -0,0 +1,14 @@
+```dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> fetchData() async {
+  try {
+    final response = await http.get(Uri.parse('https://api.example.com/data'));
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      final someValue = jsonData['someKey'];
+      if (someValue != null) {
+        print('Data: $someValue');
+      } else {
+        print('Key 
